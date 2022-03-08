0fd416f66f3d4aee8e642c714f34609d1f1e016a
Vutulosa <59256117+Vutulosa@users.noreply.github.com>
2022-03-08 11:41:05 +0000

    Fix to a screen and addded pins

--- 2122-6131COMP-BLM/src/main.cpp
+++ 2122-6131COMP-BLM/src/main.cpp
@@ -1,14 +1,29 @@
 #include <Arduino.h>
 #include <SPIFFS.h>
 #include <SD.h>
-#include <TFT_eSPI.h>                
+#include <TFT_eSPI.h>      
+#include <HTTPClient.h>
+#include <WiFi.h>
+#include <WiFiClientSecure.h>
+#include <Adafruit_Sensor.h>  
+#include <DHT.h>  
+
+//PINS : 
+// SCK = 18  **BLUE**
+// SO = 19 **ORANGE**
+// SI = 23 **GREEN**
+// TCS = 15 **ORANGE**
+// RST = 4 **PURPLE**
+// D/C = 2 **YELLOW**
+// CCS = 5 **BLACK**
+// DHT = 16 **WHITE**
 
 const int CS_PIN = 5;
+const int DHT_PIN = 16;
 String fileName = "/arduino.txt";
 
-#include <TFT_eSPI.h>                 // Include the graphics library (this includes the sprite functions)
-
-TFT_eSPI    tft = TFT_eSPI();         // Create object "tft"
+TFT_eSPI    tft = TFT_eSPI();    
+DHT dht(DHT_PIN, DHT11);
 
 void writeFile() {
   SD.begin(CS_PIN);
@@ -38,19 +53,20 @@ void readFile() {
 
 void printLine(String heading, String value) {
   tft.setTextColor(TFT_GREEN, TFT_BLACK);
-  tft.setCursor(5, 5);
   tft.print(heading + ":");
   tft.setTextColor(TFT_MAGENTA);
   tft.println(value);
 }
 
 void printOnScreen(String temperature, String humidity) {
+  tft.fillScreen(TFT_BLACK);
   // Wrap text at right and bottom of screen and set text size
   tft.setTextWrap(true, true);
-  tft.setTextSize(2);
+  tft.setTextSize(1.8);
+  tft.setCursor(0,10);
 
   // Print Temperature and humidity
-  printLine("Temp", temperature);
+  printLine("Temperature", temperature);
   printLine("Humidity", humidity);
 }
 
@@ -64,11 +80,14 @@ void setup(void) {
   tft.setRotation(0);
 
   tft.fillScreen(TFT_BLACK);
+
+  dht.begin();
 }
 
 // -------------------------------------------------------------------------
 // Main loop
 // -------------------------------------------------------------------------
 void loop() {
-  printOnScreen("22", "23");
+  printOnScreen(String(dht.readTemperature(), 0), String(dht.readHumidity(), 0));
+  delay(1000);
 }
